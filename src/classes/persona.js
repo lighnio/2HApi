8c12d4e25d1f1daeca795c43be829cf8cffbb0e8
lighnio <charlesvas16@outlook.com>
2022-03-10 14:02:18 -0600

    added class persona

---
+++ src/classes/persona.js
@@ -0,0 +1,10 @@
+class Persona{
+    constructor(IdPersona, Nombre, FechaDeNacimiento, Genero){
+        this.IdPersona = IdPersona;
+        this.Nombre = Nombre;
+        this.FechaDeNacimiento = FechaDeNacimiento;
+        this.Genero = Genero;
+    }
+}
+
+module.exports = Persona;
